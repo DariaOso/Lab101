ae49f55fe933376b940d99c335eff64ecd69bb38
DariaOso <71928567+DariaOso@users.noreply.github.com>
2020-11-08 00:07:43 +0300

    Initial commit

---
+++ src/ru/mirea/lab101/Test.java
@@ -0,0 +1,9 @@
+package ru.mirea.lab101;
+
+public class Test {
+    public static void main(String[] args) {
+        Sequence s = new Sequence();
+        s.output(3);
+        s.sort();
+    }
+}
